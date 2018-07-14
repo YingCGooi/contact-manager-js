3d24a78f12a7a92b0c28ecb046677a572304e848
YingCGooi <gooiyingchyi@gmail.com>
2018-07-14 17:53:00 -0400

    implement deepclone

--- public/javascripts/underscore.js
+++ public/javascripts/underscore.js
@@ -1,16 +1,36 @@
 _ = function(array) {
-	return {
-		without(...values) {
-			return array.filter(value => !values.includes(value));
-		},
-	}
+  return {
+    without(...values) {
+      return array.filter(value => !values.includes(value));
+    },
+  }
+}
+
+_.uniq = (array) => {
+  const uniqKeysObj = {};
+  array.forEach(value => uniqKeysObj[value] = true);
+  return Object.keys(uniqKeysObj);
+}
+
+_.deepClone = (array) => {
+  const cloned = [];
+
+  array.forEach(obj => {
+    const newObj = {};
+    for (let prop in obj) {
+      newObj[prop] = obj[prop]; // primitive types are copied 
+    }
+    cloned.push(newObj);
+  });
+
+  return cloned;
 }
 
 _.debounce = (func, delay) => {
-	let timeout;
+  let timeout;
 
-	return function() {		
-		if (timeout) clearTimeout(timeout);
-		timeout = setTimeout(() => func(...arguments), delay);
-	};
+  return function() {   
+    if (timeout) clearTimeout(timeout);
+    timeout = setTimeout(() => func(...arguments), delay);
+  };
 }
